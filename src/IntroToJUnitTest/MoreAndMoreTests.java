3478e183ff650b60aa657d01bc5231d9dc03bd37
codeavenger1 <league@WTS5.attlocal.net>
2017-08-31 20:00:52 -0700

    JUnit Test are too hard
    :(

---
+++ src/IntroToJUnitTest/MoreAndMoreTests.java
@@ -0,0 +1,89 @@
+package IntroToJUnitTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Uncomment each test and then write the method to make it pass.
+ **/
+
+public class MoreAndMoreTests {
+	public String multiply(int num1, int num2) {
+		return num1 + " x " + num2 + " = " + num1 * num2;
+	}
+
+	public boolean isPrime(double num1) {
+		for (double i = 2; i < num1 - 1; i++) {
+			for (int j = 2; j < num1; j++) {
+				if (((num1 / i) * (num1 / j)) == num1) {
+					return false;
+				}
+			}
+
+		}
+		return true;
+	}
+
+	public boolean isSquare(double num1) {
+		
+		if (((Math.sqrt(num1) * 1000) % 1000 != 0)) {
+			return false;
+		}
+		return true;
+
+	}
+	public boolean isCube(double num1) {
+		if((Math.cbrt(num1) * 1000) % 1000 != 0){
+			return false;
+		}
+		return true;
+	}
+	@Test
+	public void MultTest() {
+
+		assertEquals("10 x 0 = 0", multiply(10, 0));
+		assertEquals("10 x 10 = 100", multiply(10, 10));
+		assertEquals("8 x 11 = 88", multiply(8, 11));
+	}
+
+	@Test
+	public void PrimeTest() {
+
+		assertTrue(isPrime(3));
+		assertTrue(isPrime(5));
+		assertTrue(isPrime(541));
+		assertFalse(isPrime(4));
+		assertFalse(isPrime(12));
+		assertFalse(isPrime(527));
+
+	}
+
+	@Test
+	public void SquareTest() {
+
+		assertTrue(isSquare(4));
+		assertTrue(isSquare(144));
+		assertTrue(isSquare(64));
+		assertTrue(isSquare(10201));
+		assertTrue(isSquare(1));
+		assertFalse(isSquare(3));
+		assertFalse(isSquare(22));
+		assertFalse(isSquare(143));
+
+	}
+
+	 @Test
+	 public void CubeTest() {
+	
+	 assertTrue(isCube(27));
+	 assertTrue(isCube(216));
+	 assertTrue(isCube(729));
+	 assertTrue(isCube(1));
+	 assertFalse(isCube(3));
+	 assertFalse(isCube(22));
+	 assertFalse(isCube(143));
+	
+	 }
+
+}
--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -36,57 +36,69 @@ public class Algorithms {
 	public static String findLongestWord(List<String> words) {
 		String wordLength = "";
 		for (int i = 0; i < words.size(); i++) {
-			if(words.get(i).length() >= wordLength.length() ) {
+			if (words.get(i).length() >= wordLength.length()) {
 				wordLength = words.get(i);
 			}
 		}
 		return wordLength;
 
 	}
+
 	public static String containsSOS(List<String> morse) {
-		String sos  = "";
+		String sos = "";
 		for (int i = 0; i < morse.size(); i++) {
 			sos = sos + morse.get(i);
-			if(sos.contentEquals("... --- ...")) {
+			if (sos.contentEquals("... --- ...")) {
 				return "Contains sos message";
 			}
 		}
 		return null;
-		
+
 	}
+
 	public static Double sortScores(List<Double> results) {
 		for (int i = 0; i < results.size(); i++) {
 			double track;
-			for(int j = 1; j < results.size() - i; j++ ) {
-				if(results.get(j) < results.get(j+1)) {
+			for (int j = 1; j < results.size() - i; j++) {
+				if (results.get(j) < results.get(j + 1)) {
 					results.add(results.get(j));
 					results.remove(j);
 				}
 				System.out.println(results.get(i));
-				
+
 			}
 		}
 		return 0.0;
 	}
+
 	public static String sortDNA(List<String> dna) {
-		for (int i = 0; i < dna.size(); i++) {
-			if(dna.get(i).length() > dna.get(i++).length()) {
-				dna.add(dna.get(i));
-				dna.remove(i);
-				
+		int swaps = 0;
+		while (swaps > 0) {
+			int tracker = 0;
+			swaps = 0;
+			if (dna.get(tracker).length() > dna.get(tracker++).length()) {
+				String temp = dna.get(tracker);
+				dna.remove(tracker);
+				dna.add(tracker++, temp);
+				swaps++;
+
 			}
+			
+		}
+		for (int i = 0; i < dna.size(); i++) {
 			return dna.get(i);
+		
 		}
 		return null;
-		
 	}
+
 	public static String sortWords(List<String> words) {
 		for (int i = 0; i < words.size(); i++) {
-			if(words.get(i).compareTo(words.get(i++)) < 0) {
+			if (words.get(i).compareTo(words.get(i++)) < 0) {
 				words.add(words.get(i));
 				words.remove(i);
 			}
-			if(words.get(i).compareTo(words.get(i++)) > 0) {
+			if (words.get(i).compareTo(words.get(i++)) > 0) {
 				words.add(words.get(i++));
 				words.remove(i);
 			}
